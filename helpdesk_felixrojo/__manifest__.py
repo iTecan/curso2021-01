7504d9e8fa62a034d1e6dee77e164fa4dcb6b29e
iTecan <info@itecan.es>
2021-04-20 08:56:48 +0000

    [IMP] ADD_helpdesk_felixrojo

---
+++ helpdesk_felixrojo/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright <2021(S)> Felix Rojo>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Helpdesk Felix Rojo",
+    "version": "14.0.1.0.0",
+    "author": "Felix Rojo, Odoo Community Association (OCA)",
+    # see https://odoo-community.org/page/maintainer-role for a description of the maintainer role and responsibilities
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/helpdesk_menu.xml",
+    ],
+
+}
