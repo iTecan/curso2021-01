14faecb8fa1940ed1de10cd351b11de86dd78a75
iTecan <info@itecan.es>
2021-04-21 11:39:36 +0000

    [14.0][IMP]Metodos_ORM_felixrojo

--- helpdesk_felixrojo/models/helpdesk_ticket.py
+++ helpdesk_felixrojo/models/helpdesk_ticket.py
@@ -1,8 +1,42 @@
-from odoo import models, api, fields, _
+from odoo import models, fields, api, _
+
+class HelpdeskTicketAction(models.Model):
+    _name = 'helpdesk.ticket.action'
+    _description = 'Action'
+
+    name = fields.Char()
+    date = fields.Date()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket',
+        string='Ticket')
+    
+class HelpdeskTicketTag(models.Model):
+    _name = 'helpdesk.ticket.tag'
+    _description = 'Tag'
+
+    name = fields.Char()
+    ticket_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket',
+        relation='helpdesk_ticket_tag_rel',
+        column1='tag_id',
+        column2='ticket_id',
+        string='Tags')
 
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
     _description = 'Ticket'
+
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        relation='helpdesk_ticket_tag_rel',
+        column1='ticket_id',
+        column2='tag_id',
+        string='Tags')
+
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions')
 
     name = fields.Char(
         string='Name',
@@ -31,8 +65,7 @@ class HelpdeskTicket(models.Model):
     #Asignado
     assigned = fields.Boolean(
         string='Assigned',
-        readonly='True')
-    
+        compute='_compute_assigned')    
     #Fecha limite
     date_limit = fields.Date(
         string='Date Limit')
@@ -47,6 +80,9 @@ class HelpdeskTicket(models.Model):
         string='Preventive Action',
         help='Describe preventive actions to do')
     
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Assigned to')
     
     #Cambiar estado a asignado, visible solo con estado=nuevo
     def asignar(self):
@@ -70,3 +106,48 @@ class HelpdeskTicket(models.Model):
     def cancelado(self):
         self.ensure_one()
         self.state = 'cancelado'
+    
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = self.user_id and True or False
+    
+    #campo calculado que dentro de un ticket indica los tickets asociados a un usuario
+    ticket_qty = fields.Integer(
+        string='Ticket Qty',
+        compute='_compute_ticket_qty')
+    
+    @api.depends('user_id')
+    def _compute_ticket_qty(self):
+        for record in self:
+            other_tickets = self.env['helpdesk.ticket'].search([('user_id', '=', record.user_id.id)])
+            record.ticket_qty = len(other_tickets)
+    
+    #crear un campo nombre con etiqueta, hacer boton que cree la etiqueta con nombtre y asocie a ticket
+    tag_name = fields.Char(
+        string='Tag Name')
+    
+    def create_tag(self):
+        self.ensure_one()
+        #opcion 1
+        self.write({
+            'tag_ids': [(0,0, {'name': self.tag_name})]
+        })
+        #opcion 2
+        #tag = self.env['helpdesk.ticket.tag'].create({
+        #    'name': self.tag_name
+        #})
+        #self.write({
+        #    'tag_ids': [(4,tag.id, 0)]
+        #})
+        #opcion 3
+        #tag = self.env['helpdesk.ticket.tag'].create({
+        #    'name': self.tag_name,
+        #    'ticket_ids': [(6, 0, tag.ids)]
+        #})
+        #opcion 4
+        #tag = self.env['helpdesk.ticket-tag'].create([
+        #    'name': self.tag_name,
+        #    'ticket_ids': [(6, 0, self.ids)]
+        #])
+        self.tag_name = False
