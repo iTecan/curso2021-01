fd0cd441de62c21dd687cdda37fcb07a491452c7
iTecan <info@itecan.es>
2021-04-26 16:36:28 +0000

    Tarea 5 hecha

--- helpdesk_felixrojo/__manifest__.py
+++ helpdesk_felixrojo/__manifest__.py
@@ -13,7 +13,8 @@
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
         "views/helpdesk_menu.xml",
-        "views/helpdesk_view.xml"
+        "views/helpdesk_view.xml",
+        "views/helpdesk_tag_view.xml"
     ],
 
 }
